2b425c0d2e00c8ed182a1c2a7177514fb143686d
Chukwuemeka Mba <76162087+Chukwuemeka-Mba@users.noreply.github.com>
2022-01-17 01:18:08 -0500

    updated summary

--- src/Summary.js
+++ src/Summary.js
@@ -4,7 +4,7 @@ function Summary() {
   return (
     <div className="summary">
       <h1>Nassim Taleb</h1>
-      <p>Risk Analyst, Financial Trader</p>
+      <p> Risk Engineer, Essayist</p>
       <a href="https://fooledbyrandomness.com">fooledbyrandomness.com</a>
     </div>
   );
