901eff8e34319cf5600a61460bb79340e6bdbb05
Chukwuemeka Mba <76162087+Chukwuemeka-Mba@users.noreply.github.com>
2022-01-17 00:57:31 -0500

    final commit

--- src/Buttons.js
+++ src/Buttons.js
@@ -2,25 +2,26 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+
 function Buttons() {
   return (
     <div>
       <div className="buttons">
-        <button type="submit" className="email">
-          <FontAwesomeIcon icon={faEnvelope} className=""></FontAwesomeIcon>
-          <span>Email</span>
-        </button>
-        <button
-          href="https://linkedin.com/in/nassimtaleb"
-          type=""
-          className="linkedin"
-        >
-          <FontAwesomeIcon
-            icon={faLinkedin}
-            className="hover: blue:500"
-          ></FontAwesomeIcon>
-          <span>Linkedin</span>
-        </button>
+        <a href="https://fooledbyrandomness.com/CV.htm">
+          <button type="submit" className="email">
+            <FontAwesomeIcon icon={faEnvelope} className=""></FontAwesomeIcon>
+            <span>Email</span>
+          </button>
+        </a>
+        <a href="https://linkedin.com/in/nassimtaleb">
+          <button type="" className="linkedin">
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              className="hover: blue:500"
+            ></FontAwesomeIcon>
+            <span>Linkedin</span>
+          </button>
+        </a>
       </div>
     </div>
   );
--- src/Footer.js
+++ src/Footer.js
@@ -1,15 +1,34 @@
 import React from "react";
-import "./facebook.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import {
+  faFacebook,
+  faTwitter,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+
 function Footer() {
   return (
     <div>
       <div className="footer">
         <a href="https://facebook.com">
-          <img src="./facebook.png" alt="" />
-          Facebook
+          <FontAwesomeIcon
+            className="footer-icon"
+            icon={faFacebook}
+          ></FontAwesomeIcon>
         </a>
-        <a href="https://facebook.com">Twitter </a>
-        <a href="https://facebook.com">Instagram </a>
+        <a href="https://facebook.com">
+          <FontAwesomeIcon
+            className="footer-icon"
+            icon={faTwitter}
+          ></FontAwesomeIcon>{" "}
+        </a>
+        <a href="https://facebook.com">
+          <FontAwesomeIcon
+            className="footer-icon"
+            icon={faInstagram}
+          ></FontAwesomeIcon>{" "}
+        </a>
       </div>
     </div>
   );
